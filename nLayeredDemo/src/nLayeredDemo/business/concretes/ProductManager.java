8e495f590cdd5e70972898799c642558f3a77468
El Senpai <El Senpai@EL-SENPAI>
2021-05-05 23:06:53 +0300

    hey

---
+++ nLayeredDemo/src/nLayeredDemo/business/concretes/ProductManager.java
@@ -0,0 +1,52 @@
+package nLayeredDemo.business.concretes;
+
+import java.util.List;
+
+import nLayeredDemo.business.abstracts.ProductService;
+import nLayeredDemo.dataAccess.abstracts.ProductDao;
+import nLayeredDemo.entities.concretes.Product;
+
+public class ProductManager implements ProductService {
+	
+	private ProductDao productDao;
+
+	public ProductManager(ProductDao productDao) {
+		super();
+		this.productDao = productDao;
+	}
+
+	@Override
+	public void add(Product product) {
+		
+		productDao.add(product);
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Product product) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Product product) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Product get(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Product> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
