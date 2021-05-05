ff74372ae7e5e3bee8062c8e02a71a3ed1844a49
El Senpai <El Senpai@EL-SENPAI>
2021-05-05 23:25:21 +0300

    abc

--- nLayeredDemo/src/nLayeredDemo/Main.java
+++ nLayeredDemo/src/nLayeredDemo/Main.java
@@ -8,12 +8,13 @@ import nLayeredDemo.entities.concretes.Product;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// TODO : Spring IoC ile cozulecek
 		
 
 		ProductService productService=new ProductManager(new HibernateProductDao());
 		
-		Product product=new Product(1,1,"Elma",12,50);
+		Product product=new Product(1,1,"Elma",12,50);//business rule a takilacak
 		Product product1=new Product(1,2,"Armut",32,40);
 		productService.add(product);
 		productService.add(product1);
