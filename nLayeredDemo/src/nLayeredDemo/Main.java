fe9e6319a37aa482affc64f112d26d85a6acb53e
El Senpai <El Senpai@EL-SENPAI>
2021-05-05 23:20:43 +0300

    main

--- nLayeredDemo/src/nLayeredDemo/Main.java
+++ nLayeredDemo/src/nLayeredDemo/Main.java
@@ -1,6 +1,9 @@
 package nLayeredDemo;
 
-
+import nLayeredDemo.business.abstracts.ProductService;
+import nLayeredDemo.business.concretes.ProductManager;
+import nLayeredDemo.dataAccess.concretes.HibernateProductDao;
+import nLayeredDemo.entities.concretes.Product;
 
 public class Main {
 
@@ -8,6 +11,12 @@ public class Main {
 		// TODO Auto-generated method stub
 		
 
+		ProductService productService=new ProductManager(new HibernateProductDao());
+		
+		Product product=new Product(1,1,"Elma",12,50);
+		
+		productService.add(product);
+		
 	}
 
 }
