870b620b3675fcb03b862850f55943b5da744760
El Senpai <El Senpai@EL-SENPAI>
2021-05-05 22:58:11 +0300

    yo

--- nLayeredDemo/src/nLayeredDemo/dataAccess/abstracts/ProductDao.java
+++ nLayeredDemo/src/nLayeredDemo/dataAccess/abstracts/ProductDao.java
@@ -1,5 +1,7 @@
 package nLayeredDemo.dataAccess.abstracts;
 
+import java.util.ArrayList;
+
 import nLayeredDemo.entities.concretes.Product;
 
 public interface ProductDao {
@@ -7,6 +9,8 @@ public interface ProductDao {
 	void add(Product product);
 	void update(Product product);
 	void delete(Product product);
+	Product get(int id);
+	ArrayList<Product> getAll();
 	
 		
 	
